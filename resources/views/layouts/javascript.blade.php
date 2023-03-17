4ad794ff52b96d42c8dded82f6f36b48a8a91838
microsoftjulius <julisema4@gmail.com>
2023-03-17 12:50:01 +0300

    the ui organized

--- resources/views/layouts/javascript.blade.php
+++ resources/views/layouts/javascript.blade.php
@@ -1,25 +1,25 @@
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/jquery.min.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/jquery.min.js'}}"></script>
 <!-- Bootstrap tether Core JavaScript -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/bootstrap.bundle.min.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/bootstrap.bundle.min.js'}}"></script>
 <!-- slimscrollbar scrollbar JavaScript -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/perfect-scrollbar.jquery.min.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/perfect-scrollbar.jquery.min.js'}}"></script>
 <!--Wave Effects -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/waves.html'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/waves.html'}}"></script>
 <!--Menu sidebar -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/sidebarmenu.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/sidebarmenu.js'}}"></script>
 <!--Custom JavaScript -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/custom.min.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/custom.min.js'}}"></script>
 <!-- ============================================================== -->
 <!-- This page plugins -->
 <!-- ============================================================== -->
 <!--morris JavaScript -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/raphael-min.js'}}"></script>
-{{--<script src="{{ 'https://accounts.igsinternet.com/dist/js/morris.min.js'}}"></script>--}}
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/jquery.sparkline.min.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/raphael-min.js'}}"></script>
+{{--<script src="{{ 'http://accounts.igsinternet.com/dist/js/morris.min.js'}}"></script>--}}
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/jquery.sparkline.min.js'}}"></script>
 <!-- Popup message jquery -->
-<script src="{{ 'https://accounts.igsinternet.com/dist/js/jquery.toast.js'}}"></script>
+<script src="{{ 'http://accounts.igsinternet.com/dist/js/jquery.toast.js'}}"></script>
 <!-- Alpine v3 -->
-<script defer src="{{'https://accounts.igsinternet.com/livewire_asset/cdn.js'}}"></script>
-<link href="{{'https://accounts.igsinternet.com/livewire_asset/tailwind.css'}}" rel="stylesheet">
+<script defer src="{{'http://accounts.igsinternet.com/livewire_asset/cdn.js'}}"></script>
+<link href="{{'http://accounts.igsinternet.com/livewire_asset/tailwind.css'}}" rel="stylesheet">
 @livewireScripts
 @livewire('livewire-ui-modal')
