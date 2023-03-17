0947d93de2e6efd57c9d26a3c0f64ecb2389c9cb
microsoftjulius <julisema4@gmail.com>
2023-03-17 12:54:54 +0300

    the ui organized

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -5,9 +5,9 @@
 <!-- chartist CSS -->
 <link href="{{ 'https://accounts.igsinternet.com/morrisjs/morris.css'}}" rel="stylesheet">
 <!--Toaster Popup message CSS -->
-<link href="{{ 'https://accounts.igsinternet.com/toast-master/css/jquery.toast.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/css/jquery.toast.css'}}" rel="stylesheet">
 <!-- Morris CSS -->
-<link href="{{ 'https://accounts.igsinternet.com/admin/morrisjs/morris.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/dist/css/morris.css'}}" rel="stylesheet">
 <!-- Custom CSS -->
 <link href="{{ 'https://accounts.igsinternet.com/dist/css/style.min.css'}}" rel="stylesheet">
 <!-- Dashboard 1 Page CSS -->
