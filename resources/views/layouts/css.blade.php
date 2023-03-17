8a2f6434f4421c08ededcc4f3173bd552ed06ca5
microsoftjulius <julisema4@gmail.com>
2023-03-17 12:50:58 +0300

    the ui organized

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,16 +1,16 @@
 <!-- Favicon icon -->
-<link rel="icon" type="image/png" sizes="16x16" href="{{'http://accounts.igsinternet.com/dist/images/logo.png'}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{'https://accounts.igsinternet.com/dist/images/logo.png'}}">
 <title>Accounts - {{Request()->route()->getName()}}</title>
 <!-- This page CSS -->
 <!-- chartist CSS -->
-<link href="{{ 'http://accounts.igsinternet.com/morrisjs/morris.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/morrisjs/morris.css'}}" rel="stylesheet">
 <!--Toaster Popup message CSS -->
-<link href="{{ 'http://accounts.igsinternet.com/toast-master/css/jquery.toast.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/toast-master/css/jquery.toast.css'}}" rel="stylesheet">
 <!-- Morris CSS -->
-<link href="{{ 'http://accounts.igsinternet.com/admin/morrisjs/morris.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/admin/morrisjs/morris.css'}}" rel="stylesheet">
 <!-- Custom CSS -->
-<link href="{{ 'http://accounts.igsinternet.com/dist/css/style.min.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/dist/css/style.min.css'}}" rel="stylesheet">
 <!-- Dashboard 1 Page CSS -->
-<link href="{{ 'http://accounts.igsinternet.com/dist/css/pages/dashboard1.css'}}" rel="stylesheet">
-<link href="{{ 'http://accounts.igsinternet.com/dist/css/pages/icon-page.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/dist/css/pages/dashboard1.css'}}" rel="stylesheet">
+<link href="{{ 'https://accounts.igsinternet.com/dist/css/pages/icon-page.css'}}" rel="stylesheet">
 @livewireStyles
